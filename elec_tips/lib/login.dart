b19f286c8d85f2bb0bc74a351ada3586304b08e1
gyang6633 <gyang6633@gmail.com>
2022-04-02 18:31:10 -0500

    firebase auth working

--- elec_tips/lib/login.dart
+++ elec_tips/lib/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,39 +10,58 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 232, 214, 203),
-
-
-
-
-
       appBar: AppBar(
-
+          title: Text('User is (Logged ' + (user == null ? 'out' : 'in') + ')'),
       ),
       body: Center(
+            child: Column(
+              children: [
+                TextField(controller: emailController),
+                TextField(controller: passwordController),
+                Row(
+                  children: [
+                    //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    ElevatedButton(child: Text('Sign Up'),
+                        onPressed: () async {
+                          await Firebase.initializeApp();
+                          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                              email: emailController.text,
+                              password: passwordController.text
+                          );
+                          setState(() {
+                          });
+                        }),
+                    ElevatedButton(child: Text('Sign In'),
+                        onPressed: () async {
+                          await FirebaseAuth.instance.signInWithEmailAndPassword(
+                              email: emailController.text,
+                              password: passwordController.text,
+                          );
+                          setState(() {
+                          });
+                        }),
+                    ElevatedButton(child: Text('Log out'),
+                      onPressed: () async {
+                        await FirebaseAuth.instance.signOut();
+                        setState(() {
+                        });
+                      })
+                  ]
 
-            child: Container(
-              child: ElevatedButton(
-
-                child: new Text("Delete",
-                  style: TextStyle(color: Colors.white, fontSize: 22, fontFamily: "Quicksand"),
-                  textAlign: TextAlign.left,
-
-                ),
-                //DELETE HERE!!!!
-                onPressed: () {
-                  Navigator.pushNamed(context, '/signup');
-
-                },
-              ),
+                )
+              ],
             ),
 
-
-      ),
+     ),
 
     );
   }
--- elec_tips/lib/main.dart
+++ elec_tips/lib/main.dart
@@ -4,24 +4,32 @@ import 'package:elec_tips/signup.dart';
 import 'package:elec_tips/trips.dart';
 import 'package:elec_tips/leaderboard.dart';
 import 'package:elec_tips/efficiency.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() => runApp(MaterialApp(
-  //initialRoute: '/login', //change route to '/' when done with app so that loading screen works
-  home: LoginPage(),
-  routes: {
-    '/login': (context) => LoginPage(),
-    '/trips': (context) => TripsPage(),
-    '/leaderboard': (context) => LeaderboardPage(),
-    '/signup': (context) => SignupPage(),
-    '/efficiency': (context) => EfficiencyPage(),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyBlJI6RfoVL6S24klFAopEXMYKFfKwMuuk", // Your apiKey
+      appId: "1:791602728992:android:c16e538f01ed88a30aec45", // Your appId
+      messagingSenderId: "791602728992", // Your messagingSenderId
+      projectId: "electips-c1cbd", // Your projectId
+    ),
+  );
 
-
-
-
-
-
-  },
-));
+  runApp(MaterialApp(
+    //initialRoute: '/login', //change route to '/' when done with app so that loading screen works
+    home: LoginPage(),
+    routes: {
+      '/login': (context) => LoginPage(),
+      '/trips': (context) => TripsPage(),
+      '/leaderboard': (context) => LeaderboardPage(),
+      '/signup': (context) => SignupPage(),
+      '/efficiency': (context) => EfficiencyPage(),
+    },
+  ));
+}
 
 
 
