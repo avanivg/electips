1b8ee6f43f6e199a21928049130b3a352d4ebeed
shagundalal <dalal.shagun@gmail.com>
2022-04-02 20:45:30 -0500

    login, sign up and efficency pages

--- elec_tips/lib/login.dart
+++ elec_tips/lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:elec_tips/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -18,57 +19,193 @@ class _LoginPageState extends State<LoginPage> {
     User? user = FirebaseAuth.instance.currentUser;
 
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 232, 214, 203),
-      appBar: AppBar(
-          title: Text('User is (Logged ' + (user == null ? 'out' : 'in') + ')'),
-      ),
-      body: Center(
-            child: Stack(
-              children: <Widget>[
-            Column(
-              children: [
-                TextField(controller: emailController),
-                TextField(controller: passwordController),
-                Row(
-                  children: [
-                    //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    ElevatedButton(child: Text('Log in'),
-                        onPressed: () async {
-                          await Firebase.initializeApp();
-                          await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                              email: emailController.text,
-                              password: passwordController.text
-                          );
-                          setState(() {
-                          });
-                        }
-                        ),
-                    ElevatedButton(child: Text('Sign In'),
-                        onPressed: () async {
-                          await FirebaseAuth.instance.signInWithEmailAndPassword(
-                              email: emailController.text,
-                              password: passwordController.text,
-                          );
-                          setState(() {
-                          });
 
-                        }),
-                    ElevatedButton(child: Text('Log out'),
-                      onPressed: () async {
-                        await FirebaseAuth.instance.signOut();
-                        setState(() {
-                        });
-                        Navigator.pushNamed(context, '/navbar');
-                      })
-                  ]
+      backgroundColor: const Color.fromARGB(255, 220, 233, 196),
+
+      // appBar: AppBar(
+      //     title: Text('User is (Logged ' + (user == null ? 'out' : 'in') + ')'),
+      // ),
+
+      body: Container(
+        margin: EdgeInsets.fromLTRB(50, 50, 50, 50),
+
+        child: Stack(
+          children: <Widget>[
+            Column(
+                children:<Widget>[
+                  SizedBox(height: 50),
+
+                  Container(
+                    child: Align(
+                      child: Text("Welcome back!",
+                          style: TextStyle(color: Color.fromARGB(255, 28, 49, 68), fontSize: 30, fontFamily: "Quicksand"),
+                          textAlign: TextAlign.left),
+                    ),
+                  ),
+
+                ]),
+            Column(
+              children: <Widget>[
+
+                SizedBox(height: 170),
+
+                Container(
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Username:",
+                        style: TextStyle(color: Color.fromARGB(255, 28, 49, 68), fontSize: 24, fontFamily: "Quicksand"),
+                        textAlign: TextAlign.left),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                TextField(controller: emailController,
+                  style: TextStyle(height: 0.75),
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 254, 246, 215),
+                    isDense: true,
+                    isCollapsed: true,
+                    contentPadding: EdgeInsets.all(20.0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                    ),
+                    filled: true,
+
+                  ),
+                ),
+
+                SizedBox(height: 50),
+
+                Container(
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Password:",
+                        style: TextStyle(color: Color.fromARGB(255, 28, 49, 68), fontSize: 24, fontFamily: "Quicksand"),
+                        textAlign: TextAlign.left),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                TextField(controller: passwordController,
+                  style: TextStyle(height: 0.75),
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 254, 246, 215),
+                    isDense: true,
+                    isCollapsed: true,
+                    contentPadding: EdgeInsets.all(20.0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                    ),
+                    filled: true,
+
+                  ),
+                ),
+
+                SizedBox(height:10),
+
+                Row(
+                    children: [
+
+                      Text("New? ", style: TextStyle(color: Color.fromARGB(255, 28, 49, 68),)),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const SignupPage())
+                          );
+
+                        },
+                        child:  Text("Create account",
+                          style: TextStyle(decoration: TextDecoration.underline, color: Color.fromARGB(255, 28, 49, 68),
+
+                          ),
+                        ),
+
+                      ),
+
+
+
+
+
+
+
+                    ]
+                ),
+
+                SizedBox(height: 150),
+
+                Row(
+                    children: [
+                      //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      ElevatedButton(
+                          child: Text('Log in'),
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)
+                            ),
+                            primary: const Color.fromARGB(255, 28, 49, 68),
+                            fixedSize: const Size(290, 40),
+                          ),
+                          onPressed: () async {
+                            Navigator.pushNamed(context, '/navbar');
+                            await Firebase.initializeApp();
+                            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                                email: emailController.text,
+                                password: passwordController.text
+                            );
+                            setState(() {
+                            });
+
+                          }),
+
+                      // ElevatedButton(
+                      //     child: Text('Sign In'),
+                      //     style: ElevatedButton.styleFrom(
+                      //         shape: RoundedRectangleBorder(
+                      //             borderRadius: BorderRadius.circular(20)
+                      //         ),
+                      //         primary: const Color.fromARGB(255, 28, 49, 68),
+                      //         fixedSize: const Size(80, 40),
+                      //     ),
+                      //     onPressed: () async {
+                      //       await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      //         email: emailController.text,
+                      //         password: passwordController.text,
+                      //       );
+                      //       setState(() {
+                      //       });
+                      //
+                      //     }),
+                      // ElevatedButton(
+                      //     child: Text('Log out'),
+                      //     style: ElevatedButton.styleFrom(
+                      //       shape: RoundedRectangleBorder(
+                      //           borderRadius: BorderRadius.circular(20)
+                      //       ),
+                      //       primary: const Color.fromARGB(255, 28, 49, 68),
+                      //       fixedSize: const Size(100, 40),
+                      //     ),
+                      //     onPressed: () async {
+                      //       await FirebaseAuth.instance.signOut();
+                      //       setState(() {
+                      //       });
+                      //       Navigator.pushNamed(context, '/navbar');
+                      //     })
+                    ]
 
                 )
               ],
             ),
-],
-     ),
+          ],
+        ),
+
+
+
       ),
+
     );
+
   }
 }
-
--- elec_tips/lib/signup.dart
+++ elec_tips/lib/signup.dart
@@ -1,98 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({Key? key}) : super(key: key);
-
-
 
   @override
   _SignupPageState createState() => _SignupPageState();
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Efficiency',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: My trips',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Leaderboard',
-      style: optionStyle,
-    ),
-  ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
 
   @override
   Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 220, 233, 196),
 
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.speed_sharp),
-            label: 'Efficiency',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_location_outlined),
-            label: 'My trips',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Leaderboard',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
+      // appBar: AppBar(
+      //     title: Text('User is (Logged ' + (user == null ? 'out' : 'in') + ')'),
+      // ),
 
       body: Container(
+        margin: EdgeInsets.fromLTRB(50, 50, 50, 50),
 
         child: Stack(
-            children: <Widget>[
-
-              Column(
+          children: <Widget>[
+            Column(
                 children:<Widget>[
+                  SizedBox(height: 50),
+
                   Container(
-
-                    child: ElevatedButton(
-
-                      child: new Text("Delete",
-                        style: TextStyle(color: Colors.white, fontSize: 22, fontFamily: "Quicksand"),
-                        textAlign: TextAlign.left,
-
-                      ),
-
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/efficiency');
-
-                      },
+                    child: Align(
+                      child: Text("Create an account",
+                          style: TextStyle(color: Color.fromARGB(255, 28, 49, 68), fontSize: 30, fontFamily: "Quicksand"),
+                          textAlign: TextAlign.left),
                     ),
                   ),
 
+                ]),
+            Column(
+              children: <Widget>[
 
-                ],
+                SizedBox(height: 170),
 
-              ),
+                Container(
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Username:",
+                        style: TextStyle(color: Color.fromARGB(255, 28, 49, 68), fontSize: 24, fontFamily: "Quicksand"),
+                        textAlign: TextAlign.left),
+                  ),
+                ),
 
-            ]
+                SizedBox(height: 10),
 
+                TextField(controller: emailController,
+                  style: TextStyle(height: 0.75),
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 254, 246, 215),
+                    isDense: true,
+                    isCollapsed: true,
+                    contentPadding: EdgeInsets.all(20.0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                    ),
+                    filled: true,
+
+                  ),
+                ),
+
+                SizedBox(height: 50),
+
+                Container(
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Password:",
+                        style: TextStyle(color: Color.fromARGB(255, 28, 49, 68), fontSize: 24, fontFamily: "Quicksand"),
+                        textAlign: TextAlign.left),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                TextField(controller: passwordController,
+                  style: TextStyle(height: 0.75),
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 254, 246, 215),
+                    isDense: true,
+                    isCollapsed: true,
+                    contentPadding: EdgeInsets.all(20.0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                    ),
+                    filled: true,
+
+                  ),
+                ),
+
+
+
+
+
+                SizedBox(height: 150),
+
+                Row(
+                    children: [
+
+                      ElevatedButton(
+                          child: Text('Sign In'),
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)
+                            ),
+                            primary: const Color.fromARGB(255, 28, 49, 68),
+                            fixedSize: const Size(290, 40),
+                          ),
+                          onPressed: () async {
+                            Navigator.pushNamed(context, '/navbar');
+                            await FirebaseAuth.instance.signInWithEmailAndPassword(
+                              email: emailController.text,
+                              password: passwordController.text,
+                            );
+                            setState(() {
+                            });
+
+                          }),
+
+                    ]
+
+                )
+              ],
+            ),
+          ],
         ),
+
+
 
       ),
 
